54e95e9201532c4eaf8fa6bc15b60b4e12868e2e
kyungjinleelee <lv_jin@naver.com>
2024-09-10 16:56:09 +0900

    feat: #2 FeignClient 설정 - application.properties 설정 및 EnableFeignClient 설정

---
+++ logistics/src/main/java/msa/logistics/service/logistics/LogisticsApplication.java
@@ -0,0 +1,15 @@
+package msa.logistics.service.logistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class LogisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticsApplication.class, args);
+	}
+
+}
