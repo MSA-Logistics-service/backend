5fdda7e61c009e0b70952ef8e9546f08cbf3ed42
kyungjinleelee <lv_jin@naver.com>
2024-09-10 16:55:20 +0900

    init: #2 Logistics 서비스 생성

---
+++ logistics/src/test/java/msa/logistics/service/logistics/LogisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package msa.logistics.service.logistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
