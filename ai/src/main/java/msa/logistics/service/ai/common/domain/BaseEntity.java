671515fbcd05956f7f8f441b0cdfc8695995e377
CJ-1998 <choijun0627@konkuk.ac.kr>
2024-09-12 17:37:53 +0900

    Feat: #15 ai 서비스 공통 로직 추가
    BaseEntity 추가
    공통 응답, 예외 처리 로직 구현

---
+++ ai/src/main/java/msa/logistics/service/ai/common/domain/BaseEntity.java
@@ -0,0 +1,48 @@
+package msa.logistics.service.ai.common.domain;
+
+import static lombok.AccessLevel.PROTECTED;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity implements Serializable {
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @CreatedBy
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @LastModifiedBy
+    @Column(name = "updated_by")
+    private String updatedBy;
+
+    @Column(name = "deleted_at")
+    protected LocalDateTime deletedAt;
+
+    @Column(name = "deleted_by")
+    protected String deletedBy;
+
+    @Column(name = "is_delete")
+    protected Boolean isDelete;
+}
