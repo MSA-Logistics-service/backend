e193d4ea19e536dc33b9eaf7d809bdb1644c938b
kyungjinleelee <lv_jin@naver.com>
2024-09-13 01:07:18 +0900

    feat: #12 상품 Create 구현

--- logistics/src/main/java/msa/logistics/service/logistics/product/controller/ProductController.java
+++ logistics/src/main/java/msa/logistics/service/logistics/product/controller/ProductController.java
@@ -1,9 +1,19 @@
 package msa.logistics.service.logistics.product.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import msa.logistics.service.logistics.common.dto.ApiResponseDto;
+import msa.logistics.service.logistics.product.dto.request.ProductCreateRequestDto;
 import msa.logistics.service.logistics.product.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/products")
@@ -11,5 +21,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
     private final ProductService productService;
+
+    /**
+     * 상품 생성
+     */
+    @PostMapping
+    public ResponseEntity<ApiResponseDto<UUID>> createProduct(@Valid @RequestBody ProductCreateRequestDto request) {
+        UUID productId = productService.createProduct(request);
+        return ResponseEntity
+                .created(URI.create("/api/v1/products/" + productId))
+                .body(new ApiResponseDto<>(HttpStatus.CREATED, "상품이 생성되었습니다.", productId));
+    }
 
 }
--- logistics/src/main/java/msa/logistics/service/logistics/product/service/ProductService.java
+++ logistics/src/main/java/msa/logistics/service/logistics/product/service/ProductService.java
@@ -1,8 +1,13 @@
 package msa.logistics.service.logistics.product.service;
 
 import lombok.RequiredArgsConstructor;
+import msa.logistics.service.logistics.product.domain.Product;
+import msa.logistics.service.logistics.product.dto.request.ProductCreateRequestDto;
 import msa.logistics.service.logistics.product.repository.ProductRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -10,5 +15,18 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
+    // 상품 생성
+    @Transactional
+    public UUID createProduct(ProductCreateRequestDto request) {
+        //TODO: 리팩토링 업체와 허브의 존재 여부 확인 로직 생략
+        Product product = Product.builder()
+                .productName(request.getProductName())
+                .stockQuantity(request.getStockQuantity())
+                .vendorId(request.getVendorId())
+                .hubId(request.getHubId())
+                .build();
 
+        productRepository.save(product);
+        return product.getProductId();
+    }
 }
