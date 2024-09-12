9788a5014c38928f3a8e8a4feb2d7f32c69dc96c
CJ-1998 <choijun0627@konkuk.ac.kr>
2024-09-12 17:48:02 +0900

    Feat: #15 AI 엔티티 생성
    AI 엔티티 생성

---
+++ ai/src/main/java/msa/logistics/service/ai/domain/AI.java
@@ -0,0 +1,38 @@
+package msa.logistics.service.ai.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Size;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import msa.logistics.service.ai.common.domain.BaseEntity;
+
+@Slf4j
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "p_ais")
+public class AI extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "ai_id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Size(max = 50)
+    @Column(name = "ai_request")
+    private String aiRequest;
+
+    @Size(max = 50)
+    @Column(name = "ai_response")
+    private String aiResponse;
+
+    private Long userId;
+}
