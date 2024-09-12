fdcfec9794134d47350ede32e8d08a31efd40ec5
CJ-1998 <choijun0627@konkuk.ac.kr>
2024-09-12 16:08:18 +0900

    Feat: #7 User entity 생성
    User entity 생성
    Base Entity 생성
    User 역할인 UserRole enum 생성

---
+++ user/src/main/java/msa/logistics/service/user/domain/User.java
@@ -0,0 +1,50 @@
+package msa.logistics.service.user.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import java.io.Serializable;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+import msa.logistics.service.user.common.domain.BaseEntity;
+
+@Slf4j
+@ToString
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "p_user")
+public class User extends BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", updatable = false, nullable = false)
+    private Long id;
+
+    @Size(min = 4, max = 10)  // 길이를 4~10자로 제한
+    @Pattern(regexp = "^[a-z0-9]+$")  // 알파벳 소문자와 숫자로만 구성
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Size(min = 4, max = 10)
+    @Pattern(regexp = "^[a-z0-9]+$")
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private UserRole role;
+}
