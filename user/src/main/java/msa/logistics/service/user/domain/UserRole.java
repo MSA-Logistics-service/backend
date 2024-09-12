ab364b02ed42b49160ad1d0c23526b71aa3c26f5
CJ-1998 <choijun0627@konkuk.ac.kr>
2024-09-12 16:22:59 +0900

    Style: #7 UserRole 이름 수정
    VENDOR는 모호해서 VENDOR_MANAGER로 수정

--- user/src/main/java/msa/logistics/service/user/domain/UserRole.java
+++ user/src/main/java/msa/logistics/service/user/domain/UserRole.java
@@ -5,7 +5,7 @@ public enum UserRole {
     MASTER("MASTER"),   // 마스터 권한
     HUB_ADMIN("HUB_ADMIN"), // 허브 관리자 권한
     HUB_DELIVERY_MANAGER("HUB_DELIVERY_MANAGER"),   // 허브 배송 담당자 권한
-    VENDOR("VENDOR");   // 업체 권한
+    VENDOR_MANAGER("VENDOR_MANAGER");   // 업체 권한
 
     private final String authority;
 
