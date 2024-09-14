9d6d57c0d644088cd6435fc2bf75411a9b5352ef
lossol1 <thffo123@gmail.com>
2024-09-14 12:20:52 +0900

    common 기본설정

---
+++ hub/src/main/java/com/msa/hub/common/exception/ErrorCode.java
@@ -0,0 +1,36 @@
+package com.msa.hub.common.exception;
+
+import org.springframework.http.HttpStatus;
+
+public enum ErrorCode {
+    // ------ 4xx ------
+    NOT_FOUND(HttpStatus.BAD_REQUEST, "요청사항을 찾지 못했습니다."),
+    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+    RESPONSE_NOT_FOUND(HttpStatus.NOT_FOUND, "응답을 찾을 수 없습니다."),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "접근이 거부되었습니다."),
+    PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 상품을 찾을 수 없습니다."),
+    VENDOR_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 업체를 찾을 수 없습니다."),
+    HUB_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 허브를 찾을 수 없습니다."),
+
+
+    // ------ 5xx ------
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에 문제가 발생했습니다."),;
+
+
+    private final HttpStatus status;
+    private final String message;
+
+    ErrorCode(final HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
