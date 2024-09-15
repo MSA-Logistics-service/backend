477d3568eb155a07c9c2216359e0af041a317df7
CJ-1998 <choijun0627@konkuk.ac.kr>
2024-09-16 08:03:53 +0900

    Feat: #25 gateway 인증 필터 구현
    User로 요청 보내는 feignClient 구현 완료
    feignClient 사용해 JWT 인증하는 필터 구현 완료

--- gateway/src/main/java/msa/logistics/service/gateway/client/UserServiceClient.java
+++ gateway/src/main/java/msa/logistics/service/gateway/client/UserServiceClient.java
@@ -1,4 +1,17 @@
 package msa.logistics.service.gateway.client;
 
+import java.util.List;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient(name = "user")
+@RequestMapping("/api/v1/users")
 public interface UserServiceClient {
+    @GetMapping("/username/{username}")
+    String getUsernameByUsername(@PathVariable("username") String username);
+
+    @GetMapping("/role/{username}")
+    List<String> getRoleByUsername(@PathVariable("username") String username);
 }
--- gateway/src/main/java/msa/logistics/service/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/msa/logistics/service/gateway/config/SecurityConfig.java
@@ -2,15 +2,20 @@ package msa.logistics.service.gateway.config;
 
 import io.jsonwebtoken.Claims;
 import jakarta.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Optional;
 import msa.logistics.service.gateway.client.UserServiceClient;
 import msa.logistics.service.gateway.util.JwtUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.util.StringUtils;
+import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 
 @Configuration
@@ -22,6 +27,7 @@ public class SecurityConfig {
 
     public SecurityConfig(JwtUtil jwtUtil, UserServiceClient userServiceClient) {
         this.jwtUtil = jwtUtil;
+        this.userServiceClient = userServiceClient;
     }
 
     @Bean
@@ -49,14 +55,30 @@ public class SecurityConfig {
                 tokenValue = jwtUtil.substringToken(tokenValue);
 
                 if (!jwtUtil.validateToken(tokenValue)) {
-                    return;
+                    return chain.filter(exchange);
                 }
 
                 Claims claims = jwtUtil.getUserInfoFromToken(tokenValue);
 
                 String username = claims.getSubject();
-                
+
+                String foundUsername = Optional.ofNullable(userServiceClient.getUsernameByUsername(username))
+                        .orElseThrow(() -> new UsernameNotFoundException("User " + username + " not found"));
+
+                List<String> foundRole = Optional.ofNullable(userServiceClient.getRoleByUsername(username))
+                        .orElseThrow(() -> new UsernameNotFoundException("User " + username + " not found"));
+
+                // 사용자 정보를 새로운 헤더에 추가
+                ServerHttpRequest modifiedRequest = exchange.getRequest().mutate()
+                        .header("X-User-Name", foundUsername)  // 사용자명 헤더 추가
+                        .header("X-User-Roles", String.join(",", foundRole))    // 권한 정보 헤더 추가
+                        .build();
+
+                // 수정된 요청으로 필터 체인 계속 처리
+                ServerWebExchange modifiedExchange = exchange.mutate().request(modifiedRequest).build();
+                return chain.filter(modifiedExchange);
             }
+            return chain.filter(exchange);
         };
     }
 }
