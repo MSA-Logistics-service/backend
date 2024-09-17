9e5648d2c3cf5518aaed13458c7884ed704b38fa
CJ-1998 <choijun0627@konkuk.ac.kr>
2024-09-17 21:49:06 +0900

    Feat: #26 AuthService 구현
    AuthController에서 하던 일들 AuthService 생성해서 이동

--- auth/src/main/java/msa/logistics/service/auth/controller/AuthController.java
+++ auth/src/main/java/msa/logistics/service/auth/controller/AuthController.java
@@ -2,21 +2,13 @@ package msa.logistics.service.auth.controller;
 
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
-import java.io.IOException;
-import java.util.Collection;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
-import msa.logistics.service.auth.client.UserServiceClient;
 import msa.logistics.service.auth.dto.LoginRequestDto;
 import msa.logistics.service.auth.dto.SignUpDto;
-import msa.logistics.service.auth.security.UserDetailsImpl;
-import msa.logistics.service.auth.util.JwtUtil;
+import msa.logistics.service.auth.service.AuthService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,38 +19,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/auth")
 public class AuthController {
 
-    private final AuthenticationManager authenticationManager;
-    private final JwtUtil jwtUtil;
-    private final PasswordEncoder passwordEncoder;
-    private final UserServiceClient userServiceClient;
+    private final AuthService authService;
 
     @PostMapping("/sign-in")
-    public ResponseEntity<?> login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response)
-            throws IOException {
-
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(loginRequestDto.getUsername(), loginRequestDto.getPassword())
-        );
-
-        String username = ((UserDetailsImpl) authentication.getPrincipal()).getUsername();
-        Collection<GrantedAuthority> roles = ((UserDetailsImpl) authentication.getPrincipal()).getAuthorities();
-        List<String> rolesList = roles.stream().map(GrantedAuthority::getAuthority).toList();
-        String rolesString = rolesList.toString();
-
-        String jwt = jwtUtil.createToken(username, roles);
-        jwtUtil.addJwtToHeader(jwt, response);
-
-        response.addHeader("X-User-Id", username);
-        response.addHeader("X-User-Role", rolesString);
-
-        return ResponseEntity.ok().body(username + "login success");
+    public ResponseEntity<?> login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response) {
+        try {
+            authService.login(loginRequestDto, response);
+            return ResponseEntity.ok().body("login success");
+        } catch (AuthenticationException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Login failed");
+        }
     }
 
     @PostMapping("/sign-up")
     public ResponseEntity<?> signup(@RequestBody @Valid SignUpDto signUpDto) {
-        String password = passwordEncoder.encode(signUpDto.getPassword());
-        signUpDto.setPassword(password);
-        Boolean result = userServiceClient.registerUser(signUpDto);
+        Boolean result = authService.signup(signUpDto);
         if (result) {
             return ResponseEntity.ok().body("signup success");
         } else {
---
+++ auth/src/main/java/msa/logistics/service/auth/service/AuthService.java
@@ -0,0 +1,50 @@
+package msa.logistics.service.auth.service;
+
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.Collection;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import msa.logistics.service.auth.client.UserServiceClient;
+import msa.logistics.service.auth.dto.LoginRequestDto;
+import msa.logistics.service.auth.dto.SignUpDto;
+import msa.logistics.service.auth.security.UserDetailsImpl;
+import msa.logistics.service.auth.util.JwtUtil;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+    private final PasswordEncoder passwordEncoder;
+    private final UserServiceClient userServiceClient;
+
+    public void login(LoginRequestDto loginRequestDto, HttpServletResponse response) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequestDto.getUsername(), loginRequestDto.getPassword())
+        );
+
+        String username = ((UserDetailsImpl) authentication.getPrincipal()).getUsername();
+        Collection<GrantedAuthority> roles = ((UserDetailsImpl) authentication.getPrincipal()).getAuthorities();
+        List<String> rolesList = roles.stream().map(GrantedAuthority::getAuthority).toList();
+        String rolesString = rolesList.toString();
+
+        String jwt = jwtUtil.createToken(username, roles);
+        jwtUtil.addJwtToHeader(jwt, response);
+
+        response.addHeader("X-User-Id", username);
+        response.addHeader("X-User-Role", rolesString);
+    }
+
+    public Boolean signup(SignUpDto signUpDto) {
+        String password = passwordEncoder.encode(signUpDto.getPassword());
+        signUpDto.setPassword(password);
+        return userServiceClient.registerUser(signUpDto);
+    }
+}
