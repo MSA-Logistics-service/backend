9fc8eebe7ea4a670e8a682e57546f8149827ac14
CJ-1998 <choijun0627@konkuk.ac.kr>
2024-09-17 21:41:17 +0900

    Feat: #26 Auth 서버 회원 가입 로직 구현
    회원 가입 요청 받아서 User 서버로 요청 보내도록 구현

--- auth/src/main/java/msa/logistics/service/auth/controller/AuthController.java
+++ auth/src/main/java/msa/logistics/service/auth/controller/AuthController.java
@@ -1,11 +1,14 @@
 package msa.logistics.service.auth.controller;
 
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import msa.logistics.service.auth.client.UserServiceClient;
 import msa.logistics.service.auth.dto.LoginRequestDto;
+import msa.logistics.service.auth.dto.SignUpDto;
 import msa.logistics.service.auth.security.UserDetailsImpl;
 import msa.logistics.service.auth.util.JwtUtil;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +16,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +29,8 @@ public class AuthController {
 
     private final AuthenticationManager authenticationManager;
     private final JwtUtil jwtUtil;
+    private final PasswordEncoder passwordEncoder;
+    private final UserServiceClient userServiceClient;
 
     @PostMapping("/sign-in")
     public ResponseEntity<?> login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response)
@@ -46,6 +52,18 @@ public class AuthController {
         response.addHeader("X-User-Role", rolesString);
 
         return ResponseEntity.ok().body(username + "login success");
+    }
+
+    @PostMapping("/sign-up")
+    public ResponseEntity<?> signup(@RequestBody @Valid SignUpDto signUpDto) {
+        String password = passwordEncoder.encode(signUpDto.getPassword());
+        signUpDto.setPassword(password);
+        Boolean result = userServiceClient.registerUser(signUpDto);
+        if (result) {
+            return ResponseEntity.ok().body("signup success");
+        } else {
+            return ResponseEntity.badRequest().body("signup failed");
+        }
     }
 
 }
---
+++ auth/src/main/java/msa/logistics/service/auth/dto/SignUpDto.java
@@ -0,0 +1,26 @@
+package msa.logistics.service.auth.dto;
+
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import java.io.Serializable;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class SignUpDto implements Serializable {
+    @Size(min = 4, max = 10)  // 길이를 4~10자로 제한
+    @Pattern(regexp = "^[a-z0-9]+$")  // 알파벳 소문자와 숫자로만 구성
+    private String username;
+
+    private String password;
+
+    @Size(min = 4, max = 10)
+    @Pattern(regexp = "^[a-z0-9]+$")
+    private String nickname;
+
+    @Pattern(regexp = "MASTER|HUB_ADMIN|HUB_DELIVERY_MANAGER|VENDOR_MANAGER")
+    private String role;
+}
